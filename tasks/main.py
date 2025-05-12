0909b6d9d18b933abb28d950b06f0ee2b99828f9
Alaa Abdalzaher <143899954+alaaabdalzaher@users.noreply.github.com>
2025-05-12 15:54:31 +0300

    Add files via upload

---
+++ tasks/main.py
@@ -0,0 +1,16 @@
+import tasks_module as tm
+
+def main():
+    tm.task1()
+    tm.task2()
+    tm.task3()
+    tm.task4()
+    tm.task5()
+    tm.task6()
+    tm.task7()
+    tm.task8()
+    tm.task9()
+    tm.task10()
+
+if __name__ == "__main__":
+    main()
